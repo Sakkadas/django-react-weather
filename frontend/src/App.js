d583d1983ea89295de649f8f6ba9772ed54305a0
sakkadas <eildos95@gmail.com>
2021-10-08 23:17:10 +0300

    create hook

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import Card from "@material-ui/core/Card";
+import {makeStyles} from "@material-ui/core/styles";
+import CardContent from "@material-ui/core/CardContent";
+import TextField from "@material-ui/core/TextField";
+import WeatherAPI from "./components/weatherAPI";
+
+
+const style = makeStyles((theme) => ({
+    root: {
+        marginTop: 50,
+        display: "flex",
+        width: 550,
+        height: 250,
+    },
+    cardcss: {
+        // backgroundImage: "url(" + bgImg + ")",
+        backgroundPosition: "center",
+    },
+}));
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const classes = style();
+    const [city, setCity] = React.useState(null);
+
+    return (
+        <Grid className={classes.root} alignItems="center" container="justify">
+            <Card className={classes.cardcss}>
+                <CardContent>
+                    <TextField
+                        autoFocus
+                        label="Введите город"
+                        onChange={(e) => {
+                            setCity(e.target.value);
+                        }}
+                    />
+                    <WeatherAPI city={city}/>
+                </CardContent>
+            </Card>
+        </Grid>
+    );
 }
 
 export default App;
