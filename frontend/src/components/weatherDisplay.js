80a3725f7411954bae104c7fd36c402a9e5c1bc4
sakkadas <eildos95@gmail.com>
2021-10-09 23:19:29 +0300

    install styled, styled-icons, add icons

--- frontend/src/components/weatherDisplay.js
+++ frontend/src/components/weatherDisplay.js
@@ -2,19 +2,54 @@ import React from "react";
 import Typography from "@material-ui/core/Typography";
 import CardContent from "@material-ui/core/CardContent";
 import Box from "@material-ui/core/Box";
+import styled from 'styled-components';
+import {Windicss} from '@styled-icons/simple-icons/Windicss'
+import {Droplet} from '@styled-icons/icomoon/Droplet'
+import {Timer2} from '@styled-icons/remix-line/Timer2'
 
+
+const WindicssIcon = styled(Windicss)`
+    justify: center;
+  height: 75px;
+  width: 75px;
+`
+
+const DropletIcon = styled(Droplet)`
+    justify: center;
+  height: 75px;
+  width: 75px;  
+`
+const Timer2Icon = styled(Timer2)`
+    justify: center;
+  height: 75px;
+  width: 75px;
+`
+const windicssIcon = () => <WindicssIcon/>
+
+const dropletIcon = () => <DropletIcon/>
+
+const timer2Icon = () => <Timer2Icon/>
+
+
+const WeatherIcon = styled.img`
+  display: block;
+  height: 75px;
+  width: 75px;
+`;
 
 function Display({weatherReport}) {
-    let lon = weatherReport.coord.lon;
-    let lat = weatherReport.coord.lat;
-    let weathermain = weatherReport.weather[0].main;
-    let weatherdiscription = weatherReport.weather[0].description;
-    let temp = weatherReport.main.temp;
-    let pressure = weatherReport.main.pressure;
-    let humidity = weatherReport.main.humidity;
-    let wind = weatherReport.wind.speed;
-    let country = weatherReport.sys.country;
-    let city = weatherReport.name;
+    const lon = weatherReport.coord.lon;
+    const lat = weatherReport.coord.lat;
+    const weatherDescription = weatherReport.weather[0].description;
+    const temp = weatherReport.main.temp;
+    const pressure = weatherReport.main.pressure;
+    const humidity = weatherReport.main.humidity;
+    const wind = weatherReport.wind.speed;
+    const country = weatherReport.sys.country;
+    const city = weatherReport.name;
+
+    const icon = weatherReport.weather[0].icon;
+    const iconUrl = `https://openweathermap.org/img/w/${icon}.png`;
 
     return (
         <div>
@@ -44,8 +79,17 @@ function Display({weatherReport}) {
             <CardContent>
                 <Box display="flex" flexDirection="row-reverse">
                     <Box p={0}>
+                        <Typography variant="h4" color="textPrimary">
+                            <WeatherIcon src={iconUrl}/>
+                        </Typography>
+                    </Box>
+                </Box>
+            </CardContent>
+            <CardContent>
+                <Box display="flex" flexDirection="row-reverse">
+                    <Box p={0}>
                         <Typography variant="h6" color="textSecondary">
-                            {weatherdiscription}
+                            {weatherDescription}
                         </Typography>
                     </Box>
                 </Box>
@@ -54,17 +98,21 @@ function Display({weatherReport}) {
                 <Box display="flex" flexDirection="row">
                     <Box p={1}>
                         <Typography variant="h6" color="textPrimary">
-                            Влажность: {humidity} %
+                            Влажность: {humidity}
+                            %
+                            <DropletIcon/>
                         </Typography>
                     </Box>
                     <Box p={1}>
                         <Typography variant="h6" color="textPrimary">
                             Давление: {pressure} Па
+                            <Timer2Icon/>
                         </Typography>
                     </Box>
                     <Box p={1}>
                         <Typography variant="h6" color="textPrimary">
                             Ветер: {wind} км/ч
+                            <WindicssIcon/>
                         </Typography>
                     </Box>
                 </Box>
