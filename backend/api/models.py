b4bd9377f45abd9ab826cdf3c7ce08793aec786d
sakkadas <eildos95@gmail.com>
2021-10-09 18:50:08 +0300

    create `Weather` model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class Weather(models.Model):
+    id = models.CharField(max_length=20)
+    city = models.CharField(max_length=120)
+    country = models.TextField(max_length=50)
+    temperature = models.IntegerField(default=0)
+    weather_description = models.CharField(max_length=120)
+    humidity = models.IntegerField(default=0)
+    pressure = models.IntegerField(default=0)
+    wind = models.IntegerField(default=0)
+    longitude = models.CharField(max_length=20)
+    latitude = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.city
+
+    @property
+    def to_dict(self):
+        data = {
+            'id': self.id,
+            'city': self.city,
+            'country': self.country,
+            'temperature': self.temperature,
+            'weather_description': self.weather_description,
+            'humidity': self.humidity,
+            'pressure': self.humidity,
+            'wind': self.wind,
+            'longitude': self.longitude,
+            'latitude': self.latitude,
+        }
+        return data
