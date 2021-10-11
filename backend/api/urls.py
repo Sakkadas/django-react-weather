e13410599336e7020abaf34d4bd934e0616157f3
sakkadas <eildos95@gmail.com>
2021-10-12 01:35:00 +0300

    create `Add` api view, url path

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import GetWeather, RemoveCity
+from .views import GetWeather, Add, RemoveCity
 
 urlpatterns = [
     path('', GetWeather.as_view()),
+    path('add/', Add.as_view()),
     path('remove/', RemoveCity.as_view())
 ]
