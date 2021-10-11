de51a36883f781ed881859b27aa0d8f6a76941b9
sakkadas <eildos95@gmail.com>
2021-10-12 01:36:59 +0300

    blank = true

--- backend/api/models.py
+++ backend/api/models.py
@@ -2,18 +2,17 @@ from django.db import models
 
 
 class Weather(models.Model):
-    id_city = models.CharField(max_length=20)
     city = models.CharField(max_length=120)
-    country = models.TextField(max_length=50)
-    temperature = models.IntegerField(default=0)
-    feels_like = models.IntegerField(default=0)
-    weather_description = models.CharField(max_length=120)
-    humidity = models.IntegerField(default=0)
-    pressure = models.IntegerField(default=0)
-    wind = models.IntegerField(default=0)
-    longitude = models.CharField(max_length=20)
-    latitude = models.CharField(max_length=20)
-    weather_icon = models.CharField(max_length=6)
+    country = models.TextField(max_length=50, blank=True)
+    temperature = models.IntegerField(default=0, blank=True)
+    feels_like = models.IntegerField(default=0, blank=True)
+    weather_description = models.CharField(max_length=120, blank=True)
+    humidity = models.IntegerField(default=0, blank=True)
+    pressure = models.IntegerField(default=0, blank=True)
+    wind = models.IntegerField(default=0, blank=True)
+    longitude = models.CharField(max_length=20, blank=True)
+    latitude = models.CharField(max_length=20, blank=True)
+    weather_icon = models.CharField(max_length=6, blank=True)
 
     def __str__(self):
         return self.city
@@ -21,7 +20,6 @@ class Weather(models.Model):
     @property
     def to_dict(self):
         data = {
-            'id_city': self.id_city,
             'city': self.city,
             'country': self.country,
             'temperature': self.temperature,
