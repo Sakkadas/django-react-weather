4b46e55ee85ed9f3a01aff5fdbd243f913f081ba
sakkadas <eildos95@gmail.com>
2021-10-13 23:55:33 +0300

    create footer

---
+++ frontend/src/components/footer.js
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Link from '@mui/material/Link';
+import {WeatherCloudy} from '@styled-icons/fluentui-system-regular/WeatherCloudy'
+
+
+function Copyright() {
+    return (
+        <Typography variant="body2" color="text.secondary">
+            {'Copyright © '}
+            <Link color="inherit" href="https://github.com/sakkadas">
+                <span style={{fontSize:'16px'}}><b>Sakkadas</b></span>
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
+}
+
+export default function StickyFooter() {
+    return (
+        <Box
+            sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                minHeight: '83vh',
+            }}
+        >
+            <CssBaseline/>
+            <Box
+                component="footer"
+                sx={{
+                    py: 3,
+                    px: 2,
+                    mt: 'auto',
+                    backgroundColor: (theme) =>
+                        theme.palette.mode === 'light'
+                            ? theme.palette.grey[100]
+                            : theme.palette.grey[400],
+                }}
+            >
+                <Container maxWidth="sm">
+                    <Typography variant="body1">
+                       <span style={{fontSize:'18px'}}>My Django React Weather App.</span> <WeatherCloudy style={{width:'20%'}}/>
+                    </Typography>
+                    <Copyright/>
+                </Container>
+            </Box>
+        </Box>
+    );
+}
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import StickyFooter from './components/footer'
 
 ReactDOM.render(
     <React.StrictMode>
         <App/>
+        <StickyFooter/>
     </React.StrictMode>,
     document.getElementById('root')
 );
