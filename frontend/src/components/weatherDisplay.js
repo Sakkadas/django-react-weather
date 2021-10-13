4d776f9c88e97699ccbfb9fc5733e2a4b5874bd9
sakkadas <eildos95@gmail.com>
2021-10-14 00:38:11 +0300

    Create Delete button, add icon, make styles

--- frontend/src/components/weatherDisplay.js
+++ frontend/src/components/weatherDisplay.js
@@ -1,6 +1,5 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
-
 import Typography from "@material-ui/core/Typography";
 import CardContent from "@material-ui/core/CardContent";
 import Box from "@material-ui/core/Box";
@@ -9,6 +8,8 @@ import {Windicss} from '@styled-icons/simple-icons/Windicss'
 import {Droplet} from '@styled-icons/icomoon/Droplet'
 import {Timer2} from '@styled-icons/remix-line/Timer2'
 import Button from '@mui/material/Button';
+
+import {Delete} from '@styled-icons/material-rounded/Delete';
 
 require('dotenv').config()
 
@@ -36,14 +37,16 @@ const WeatherIcon = styled.img`
 
 function handleRemoveClick(city_name) {
     axios.post("http://127.0.0.1:8000/api/remove/", {city: city_name})
+    let child = document.getElementById(city_name)
+
 }
 
 const WeatherDisplay = (props) => {
-    const UNSPLASH_API_KEY = process.env.UNSPLASH_API_KEY
-
+    const REACT_APP_apiKeyUnsplash = process.env.REACT_APP_UNSPLASH_API_KEY
     const Unsplash_url = `https://api.unsplash.com/search/photos?query=${props.city.city}&client_id=` +
-        UNSPLASH_API_KEY
-    console.log(UNSPLASH_API_KEY)
+        REACT_APP_apiKeyUnsplash
+    // UNSPLASH_API_KEY
+    console.log(REACT_APP_apiKeyUnsplash)
     const icon = props.city.weather_icon
     const iconUrl = `https://openweathermap.org/img/w/${icon}.png`;
 
@@ -131,11 +134,15 @@ const WeatherDisplay = (props) => {
                     </Box>
                 </Box>
             </CardContent>
-            <Button>
-                <div onClick={() => {
-                    handleRemoveClick(props.city.city)
-                }}>Remove
-                </div>
+            <Button style={{backgroundColor: '#901B02', color: '#F1F1F1'}}
+                    type='submit'
+                    color="primary"
+                    variant="contained"
+                    onClick={() => {
+                        handleRemoveClick(props.city.city)
+                    }}>
+                Delete
+                <Delete style={{width: "20px"}}/>
             </Button>
         </div>
     );
